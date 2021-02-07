66dc51ce3606a244eb14e4449c75171631e3bdcc
Oleksii <lexa.kiwik@gmail.com>
2021-02-07 13:14:59 +0200

    Без перечеркивания выполненых пунктов

---
+++ ToDoList/Models/Items.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace ToDoList.Models
+{
+    public class Items
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }  //Название
+
+        [DataType(DataType.Date)]
+        public DateTime ExecutionDate { get; set; } //Дата и время когда нужно сделать
+        public string Description { get; set; } //Что нужно сделать
+        //public bool IsDone { get; set; } 
+    }
+}
